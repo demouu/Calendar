38872e53ac49cdb895938b3f658adc9cd7f7e68e
Jack <jckkempton@gmail.com>
2020-10-14 18:30:50 -0600

    added moment js to javascript

--- script.js
+++ script.js
@@ -4,3 +4,6 @@
 // Dynamically update Calendar. If time has passed, turn textbox grey. If current time, turn red. If future time, turn green.
 // Create save buttons. When clicked, saves textbox to localStorage.
 
+var currentDay = $("#currentDay");
+currentDay.text(moment().format("dddd, MMM Do YYYY"));
+console.log(currentDay);

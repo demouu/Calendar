6de8ff933136d1ad764267bbc718357d3076b989
Jack <jckkempton@gmail.com>
2020-10-14 18:20:17 -0600

    added time blocks to html

---
+++ script.js
@@ -0,0 +1,6 @@
+// Get Date
+// Dynamically update Date
+// Get time
+// Dynamically update Calendar. If time has passed, turn textbox grey. If current time, turn red. If future time, turn green.
+// Create save buttons. When clicked, saves textbox to localStorage.
+
